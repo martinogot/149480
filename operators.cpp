dc9d4ec2b7213a10767ab59beb73ec194ff0957c
martinogot <martinogot77@gmail.com>
2026-07-27 20:33:37 +0300

    third ccommit

--- operators.cpp
+++ operators.cpp
@@ -12,6 +12,29 @@ int main(){
     cout << "Multiplication e.g. " << num1 << " * " << num2 << " = " << num1 * num2 << endl;
     cout << "Division e.g. " << num1 << " / " << num2 << " = " << num1/num2 << endl;
     cout << "Modulus e.g. " << num1 << " % " << num2 << " = " << num1%num2 << endl;
+    cout << endl;
+    cout << " Assiggnment Operators " << endl;
+    cout << "Simple assignment (=) " << endl;
+    cout << "Add and assign (+=) e.g. " << num1 << " += " << num2 << " = " << (num1 += num2) << endl;
+    cout << "Subtract and assign (-=) e.g. " << num1 << " -= " << num2 << " = " << (num1 -= num2) << endl;
+    cout << "etc " << endl;
+    cout << endl; 
+    cout << " Comparison/Relational Operators " << endl;
+    cout << " Greater than (>) e.g. " << num1 << " > " << num2 << " = " << (num1 > num2) << endl;
+    cout << " Less than (<) e.g. " << num2 << " < " << num1 << " = " << (num1 < num2) << endl;
+    cout << " Greater than or equal to (>=) e.g. " << num1 << " >= " << num2 << " = " << (num1 >= num2) << endl;
+    cout << " Less than or equal to (<=) e.g. " << num2 << " <= " << num1 << " = " << (num1 <= num2) << endl;
+    cout << " Equal to (==) e.g. " << num1 << " == " << num2 << " = " << (num1 == num2) << endl;
+    cout << " Not equal to (!=) e.g. " << num1 << " != " << num2 <<" = " << (num1 != num2) << endl;
+    cout << endl; 
+    cout << " Boolean/Logical Operators " << endl;
+    cout << " Logical AND (&&) e.g. " << num1 << " && " << num2 << " = " << (num1&&num2) << endl;
+    cout << " Logical OR (||) e.g. " << num1 << " || " << num2 << " = " << (num1||num2) << endl;
+    cout << " Logical NOT (!) e.g. !(" << num1 << ") = " << (!num1) << endl;
+    cout << endl; 
+    cout << " Pointer Operators " << endl;
+    int* num3;
+    cout << " Address of (&) e.g. &" << num1 << " = " << &num1 << endl;
+    cout << " Dereference (*) e.g. *" << &num1 << " = " << (*&num1) << endl;
 
-    
 }
